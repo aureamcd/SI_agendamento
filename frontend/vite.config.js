0b8eee992291a333040a2740c975f7fe50faf938
aureamcd <157396271+aureamcd@users.noreply.github.com>
2026-01-06 00:00:04 -0300

    fix: allow render host in vite server and preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    allowedHosts: ["si-agendamento.onrender.com"],
+  },
+
   preview: {
     allowedHosts: ["si-agendamento.onrender.com"],
   },
